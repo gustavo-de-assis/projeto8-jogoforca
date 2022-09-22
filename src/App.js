a9c47be359f144d992c83d9fa782b6f9468f283a
Gustavo de Assis <gustavo.siqueira.421@gmail.com>
2022-09-22 14:50:58 -0300

    Sorteio de palavras

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { useState } from "react";
+import palavras from "./palavras"
+
+
 export default function App() {
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    
+    const [palavraSorteada, setPalavra] = useState("_ _ _ _ _");
+    console.log(palavraSorteada);
+
+    function sorteiaPalavra(){
+        const valor = Math.floor(Math.random() * palavras.length);
+        const novaPalavra = palavras[valor];
+        console.log(valor)
+        setPalavra(novaPalavra);
+    }
 
     return (
         <div className="content">
@@ -11,16 +25,16 @@ export default function App() {
                 </div>
                 <div className="lado-direito">
                     <div className="escolher-palavra">
-                        <div> <strong>Escolher Palavra</strong></div>
+                        <div onClick={sorteiaPalavra}> <strong>Escolher Palavra</strong></div>
                     </div>
                     <div className="palavra-escolhida">
-                        <h1><strong>_ _ _ _ _</strong></h1>
+                        <h1><strong>{palavraSorteada}</strong></h1>
                     </div>
                 </div>
             </div>
             <div className="embaixo">
                 <ul className="alfabeto">
-                    {alfabeto.map((a) => <li className="letra-disponivel"><strong>{a.toUpperCase()}</strong></li>)}
+                    {alfabeto.map((a) => <li className="letra-indisponivel" ><strong>{a.toUpperCase()}</strong></li>)}
                 </ul>
                 <div className="chute">
                     <p>Já sei a palavra!</p>
