22c175b6c1b45ef5688196895a5f1f74bff4684e
Gustavo de Assis <gustavo.siqueira.421@gmail.com>
2022-09-25 18:54:07 -0300

    Voltado ao estado sem componentizar: Pré-jogo finalizado

--- src/App.js
+++ src/App.js
@@ -1,73 +1,61 @@
 import { useState } from "react";
 import palavras from "./palavras"
 
-function Forca() {
-    return (
-        <div className="lado-esquerdo">
-            <div className="estado-forca">
-                <img src="./assets/forca0.png" />
-            </div>
-        </div>
-    )
-}
-
-function Palavra() {
-    const [palavraSorteada, setPalavra] = useState("_ _ _ _ _");
-    const [palavraEscondida, setEscondida] = useState("");
-
-    function sorteiaPalavra() {
-        const valor = Math.floor(Math.random() * palavras.length);
-        const palavraSorteada = palavras[valor];
-        const escondePalavra = [...palavraSorteada].map((s)=>s="_ ");
-        
-        setPalavra(palavraSorteada);
-        setEscondida(escondePalavra);
-    }
-
-    return (
-        <div className="lado-direito">
-            <div className="escolher-palavra">
-                <div onClick={sorteiaPalavra}> <strong>Escolher Palavra</strong></div>
-            </div>
-            <div className="palavra-escolhida">
-                <h1><strong>{palavraEscondida}</strong></h1>
-            </div>
-        </div>
-    )
-}
-
-function Alfabeto() {
-    const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
-    return (
-        <ul className="alfabeto">
-            {alfabeto.map((a) => <li className="letra-indisponivel" ><strong>{a.toUpperCase()}</strong></li>)}
-        </ul>
-    )
-}
-
-function Chute() {
-    return (
-        <div className="chute">
-            <p>Já sei a palavra!</p>
-            <input type='text' />
-            <button>Chutar</button>
-        </div>
-    )
-}
-
 
 export default function App() {
 
+    const alfabeto = ["a", "b", "c", "d", "e", "f",
+        "g", "h", "i", "j", "k", "l", "m", "n", "o",
+        "p", "q", "r", "s", "t", "u", "v", "w", "x",
+        "y", "z"];
+
+    
+    const [palavraSorteada, setPalavra] = useState("");
+    const [palavraEscondida, setEscondida] = useState("");
+    const [botaoPalavra, setEstado] = useState(true);
+    const [letrasAlfabeto, setLetras] = useState(false);
+
+    function sorteiaPalavra() {
+        if (botaoPalavra) {
+            const valor = Math.floor(Math.random() * palavras.length);
+            const palavraSorteada = palavras[valor];
+            const escondePalavra = [...palavraSorteada].map((s) => s = "_ ");
+
+            setEstado(false);
+            setLetras(true);
+            setPalavra(palavraSorteada);
+            setEscondida(escondePalavra);
+
+        }
+    }
+
+  
     return (
         <div className="content">
             <div className="cima">
-                <Forca />
-                <Palavra />
+                <div className="lado-esquerdo">
+                    <div className="estado-forca">
+                        <img src="./assets/forca0.png" />
+                    </div>
+                </div>
+                <div className="lado-direito">
+                    <div className="escolher-palavra">
+                        <div onClick={sorteiaPalavra}> <strong>Escolher Palavra</strong></div>
+                    </div>
+                    <div className="palavra-escolhida">
+                        <h1><strong>{palavraEscondida}</strong></h1>
+                    </div>
+                </div>
             </div>
             <div className="embaixo">
-                <Alfabeto />
-                <Chute/>
+                <ul className="alfabeto">
+                    {alfabeto.map((a) => <li className={letrasAlfabeto ? "letra-disponivel":"letra-indisponivel"} ><strong>{a.toUpperCase()}</strong></li>)}
+                </ul>
+                <div className="chute">
+                    <p>Já sei a palavra!</p>
+                    <input type='text' />
+                    <button>Chutar</button>
+                </div>
             </div>
 
         </div>
