dd6b394216b9dcd1d6ba85551051bf0582f76b8b
Gustavo de Assis <gustavo.siqueira.421@gmail.com>
2022-09-26 14:01:56 -0300

    jogo finalizado ver 1.0

--- src/App.js
+++ src/App.js
@@ -8,24 +8,27 @@ export default function App() {
         "g", "h", "i", "j", "k", "l", "m", "n", "o",
         "p", "q", "r", "s", "t", "u", "v", "w", "x",
         "y", "z"];
-    
-    
-    
+
+
+
     const [palavraSorteada, setPalavra] = useState("");
     const [palavraEscondida, setEscondida] = useState("");
-    
+
     const [botaoPalavra, setBotaoPalavra] = useState(true);
     const [letrasAlfabeto, setAlfabeto] = useState(false);
-    
+
     const [letraEscolhida, setLetra] = useState([]);
     const [habilitaLetras, setEstilo] = useState("letra-desabilitada");
+    
 
     const [textoInput, setTextoInput] = useState("");
-    const [corLetras, setCorLetras] = useState ("");
+    const [corLetras, setCorLetras] = useState("");
 
-    
+
     const [qtdErro, setErro] = useState(0);
     const [qtdAcerto, setAcerto] = useState(0);
+
+    const [fimDeJogo, setFim] = useState(true);
 
 
     function sorteiaPalavra() {
@@ -41,55 +44,66 @@ export default function App() {
         }
     }
 
-    function iniciaJogo(){
+    function iniciaJogo() {
         setBotaoPalavra(false);
         setAlfabeto(true);
         setEstilo("letra-habilitada");
+        setFim(false);
     }
 
-    function escolheuLetra(botao, i){
+    function escolheuLetra(botao, i) {
         if (!letraEscolhida.includes(i))
-            setLetra([...letraEscolhida, i]);
+            setLetra([...letraEscolhida, botao]);
     }
 
-    function chuteLetra(botao, i){
+    function chuteLetra(botao, i) {
         const palavra = [...palavraSorteada.normalize('NFKD').replace(/[^\w\s.-_\/]/g, '')]; //removendo acentos para checagem
         const escondida = palavraEscondida;
+        
+        if (!fimDeJogo && !letraEscolhida.includes(botao)) {
+            if (palavra.includes(botao)) {
+                let acerto = 0;
+                const indice = palavra.map((l, i) => l.includes(botao) ? escondida[i] = palavraSorteada[i] : false);
+                palavra.map((l, i) => l.includes(botao) ? acerto++ : false);
 
-        if(palavra.includes(botao) && letrasAlfabeto) {
-            let acerto = 0;
-            const indice = palavra.map((l,i)=> l.includes(botao) ? escondida[i] = palavraSorteada[i] : false);
-            palavra.map((l,i)=> l.includes(botao) ? acerto++ : false);
+                let acertoAtual = qtdAcerto + acerto;
+                setAcerto(acertoAtual);
 
-            console.log(indice);
-            console.log(escondida);
-            console.log('acertou');
-            setAcerto((qtdAcerto + acerto));
-
-            if(qtdAcerto === palavra.length){
-                setCorLetras("acertou")
+                if (acertoAtual === palavra.length) {
+                    setCorLetras("acertou")
+                    setFim(true);
+                }
+            } else {
+                let erroAtual = qtdErro+1;
+                setErro(erroAtual)
+                if (erroAtual === 6) {
+                    setEscondida(palavraSorteada);
+                    setCorLetras("errou");
+                    setAlfabeto(false);
+                    setFim(true);
+                }
             }
-        }else{
-            console.log('errou');
-            setErro((qtdErro + 1))
-            if(qtdErro === 6){
-                setEscondida(palavraSorteada);
-                setCorLetras("errou");
-                setAlfabeto(false);
-            }
+            escolheuLetra(botao, i);
+
         }
-        escolheuLetra(botao, i);
     }
 
-    function inputChute(e){
+    function inputChute(e) {
         setTextoInput(e.target.value)
+        
     }
 
-    function chutePalavra(){
-        if(textoInput === palavraSorteada)
-            console.log('acertou miserávi');
-        else
-            console.log('errou miseravi');
+    function chutePalavra() {
+        if (textoInput === palavraSorteada) {
+            setAcerto(palavraSorteada.length);
+            setCorLetras("acertou");
+        }
+        else {
+            setCorLetras("errou");
+            setErro(6);
+        }
+        setEscondida(palavraSorteada);
+        setFim(true);
     }
 
     return (
@@ -110,14 +124,15 @@ export default function App() {
                 </div>
             </div>
             <div className="embaixo">
-                <ul className= "alfabeto" >
-                    {alfabeto.map((a, i) => <li onClick={()=> chuteLetra(a,i)} 
-                    className={letraEscolhida.includes(i) ? "letra-escolhida" : habilitaLetras}>
+                <ul className="alfabeto" >
+                    {alfabeto.map((a, i) => <li onClick={() => chuteLetra(a, i)}
+                        className={(letraEscolhida.includes(a) && !fimDeJogo
+                        ) ? "letra-escolhida" : habilitaLetras}>
                         <strong>{a.toUpperCase()}</strong></li>)}
                 </ul>
-                <div className="chute">
+                <div className={`chute ${fimDeJogo ? "escondido": ""}` }>
                     <p>Já sei a palavra!</p>
-                    <input type='text' onChange={inputChute}/>
+                    <input type='text' onChange={inputChute} />
                     <button onClick={chutePalavra}>Chutar</button>
                 </div>
             </div>
